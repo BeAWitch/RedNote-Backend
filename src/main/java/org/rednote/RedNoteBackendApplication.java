4ebb0c099f171564fb36a31170952f10018957d1
BeAWitch <109846146+BeAWitch@users.noreply.github.com>
2025-11-12 15:54:05 +0800

    项目初始化

---
+++ src/main/java/org/rednote/RedNoteBackendApplication.java
@@ -0,0 +1,13 @@
+package org.rednote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedNoteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedNoteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rednote/rednotebackend/RedNoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rednote.rednotebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedNoteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
