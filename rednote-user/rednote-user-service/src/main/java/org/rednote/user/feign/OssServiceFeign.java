6f3d3395fea8022b78c0b709f3b081d0071d47f4
BeAWitch <2213803434@qq.com>
2026-04-22 08:33:43 +0800

    fix# 保存头像时远程调用错误。

--- rednote-user/rednote-user-service/src/main/java/org/rednote/user/feign/OssServiceFeign.java
+++ rednote-user/rednote-user-service/src/main/java/org/rednote/user/feign/OssServiceFeign.java
@@ -3,11 +3,12 @@ package org.rednote.user.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient(name = "oss-service")
 public interface OssServiceFeign {
 
     @PostMapping(value = "/web/oss/uploadFile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    String uploadFile(MultipartFile file);
+    String uploadFile(@RequestPart("file") MultipartFile file);
 }
